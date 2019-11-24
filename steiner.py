815c1e619434091c05bf2e8e27e8721c664f501f
josé <jose@muttdata.ai>
2019-11-24 04:48:02 -0300

    floyd pirata

--- steiner.py
+++ steiner.py
@@ -1,10 +1,12 @@
 import numpy as np
+from scipy.sparse.csgraph import floyd_warshall
 from constants import DEFAULT_WEIGHT_MATRIX_1
 
 
 def floyd(n, w_m):
     # TODO agregar predecesor(es) al output de esta fun
     d = np.zeros((n,n))
+    p = np.zeros((n,n))
     for i in range(n):
         for j in range(n):
             if i != j:
@@ -16,3 +18,7 @@ def floyd(n, w_m):
                 d[i,j] = min(d[i,j], d[i,k]+d[k,j])
 
     return d
+
+   
+if __name__ == "__main__":
+    floyd_warshall(DEFAULT_WEIGHT_MATRIX_1, directed=False, return_predecessors=True)
