bdcfa0696e2ef20d5431e6a5b0c4d951e333342a
josé <jose@muttdata.ai>
2019-11-28 18:29:11 -0300

    add generate_new_cap_m@max_flow.py

--- max_flow.py
+++ max_flow.py
@@ -2,9 +2,7 @@ import numpy as np
 
 from constants import CAPACITY_MATRIX, DEMAND_MATRIX, WEIGHT_MATRIX_1
 
-capacidad_excedente = CAPACITY_MATRIX - DEMAND_MATRIX
-
-def edge_list_from_matrix(w_m):
+def edge_dict_from_matrix(w_m):
     n = w_m.shape[1]
     rv = []
     for i in range(n):
@@ -15,24 +13,29 @@ def edge_list_from_matrix(w_m):
     
     return {i : rv[i] for i in range(len(rv))}
 
-def generate_gp(cap_m=CAPACITY_MATRIX, dem_m=DEMAND_MATRIX, source=0, sink=1):
+def generate_new_cap_m(cap_m=CAPACITY_MATRIX, dem_m=DEMAND_MATRIX, source=0, sink=1):
     n = len(cap_m)
-    rv = np.zeros((n+2, n+2))
-    rv[0,n] = np.inf
-    rv[2:, 2:] = cap_m - dem_m
-    breakpoint()
+    rvm = cap_m - dem_m
+    rvm[n-1,0] = np.inf
+    s = []
+    t = [np.nan]
     for i in range(n):
-        rv[i, n-1] = sum(dem_m[i])
-        rv[i, n] = 'sas'
+        t.append(dem_m[i][~np.isnan(dem_m[i])].sum())
+        s.append(dem_m[:,i][~np.isnan(dem_m[:,i])].sum())
+    
+    rvm = np.r_[[s], rvm]
+    rvm = np.c_[rvm, np.array(t)]
+    breakpoint()
+    vacios_1 = np.empty((1,n+1))*np.nan
+    vacios_2 = np.empty((n+2, 1))*np.nan
+    rvm = np.r_[rvm, vacios_1]
+    rvm = np.c_[vacios_2, rvm]
 
-
-breakpoint()
-
-
-
+    return rvm
 
 if __name__ == "__main__":
-    print(edge_list_from_matrix(CAPACITY_MATRIX))
-    print(edge_list_from_matrix(DEMAND_MATRIX))
-    print(edge_list_from_matrix(WEIGHT_MATRIX_1))
-    generate_gp()
+    print(edge_dict_from_matrix(CAPACITY_MATRIX))
+    print(edge_dict_from_matrix(DEMAND_MATRIX))
+    print(edge_dict_from_matrix(WEIGHT_MATRIX_1))
+    rvm = generate_new_cap_m()
+    print(rvm)
