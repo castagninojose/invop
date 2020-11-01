4d6f36997d05167ced4957f055928f21b2adbd8d
puff <jose@muttdata.ai>
2020-11-01 12:37:09 -0300

    fix add()

--- graphs/graph.py
+++ graphs/graph.py
@@ -7,23 +7,27 @@ TODO: add support for inputs:
     scipy sparse matrix
 
 """
+from collections import defaultdict
+
+
+
 class Graph:
 
     def __init__(self, edges, directed=False):
         self._dict = defaultdict(set)
         self.directed = directed
+        self.size = len(self._dict.keys())
         self.add_edges(edges)
 
     def add_edges(self, edges):
         for n1, n2 in edges:
             self.add(n1, n2)
 
-    def add(node_1, node_2):
-        self._dict[node_1] = node_2
+    def add(self, node_1, node_2):
+        self.size += 1
+        self._dict[self.size].update({node_1, node_2})
 
 
 if __name__ == '__main__':
-    g = Graph({0: {1, 2}, 1: {2, 3}, 2: {1, 3}, 3: {3, 4}})
-    breakpoint()
-    print(g.edges)
-    print(g.nodes)
+    g = Graph([[1, 2], [2, 3], [1, 3], [3, 4]])
+    print(g._dict)
