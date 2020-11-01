900e84e8255e010dec2a844cd06f672691ef842b
puff <jose@muttdata.ai>
2020-11-01 13:36:03 -0300

    nodes property

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,10 +1,10 @@
 """Graph class.
-TODO: add support for inputs:
-    dict of dicts,
-    dict of lists,
-    numpy.matrix,
-    2d numpy ndarray,
-    scipy sparse matrix
+support for inputs:
+    [ x ] dict of sets,
+    [ ] np.ndarray / np.matrix,
+    [ ] sp.sparse,
+    [ ] dict of dicts,
+    [ ] dict of lists
 
 """
 from collections import defaultdict
@@ -15,7 +15,7 @@ class Graph:
 
     def __init__(self, edges, directed=False):
         self._dict = defaultdict(set)
-        self.directed = directed
+        self._directed = directed
         self.size = len(self._dict.keys())
         self.add_edges(edges)
 
@@ -27,7 +27,12 @@ class Graph:
         self.size += 1
         self._dict[self.size].update({node_1, node_2})
 
+    @property
+    def nodes(self):
+        return set.union(*self._dict.values())
+
 
 if __name__ == '__main__':
     g = Graph([[1, 2], [2, 3], [1, 3], [3, 4]])
+    g.add('puff', 1)
     print(g._dict)
